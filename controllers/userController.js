9f5b7018f7276d06b50e22112e36da4ca8e64a0c
Carmen Regina <crcarmen23@gmail.com>
2023-11-14 15:38:47 -0600

    completed user controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,24 +8,34 @@ module.exports = {
             .catch((err) => res.status(500).json(err));
     },
     getSingleUser(req, res) {
-        User.findOne({})
+        User.findById(req.params.userId).populate("thoughts").populate("friends")
             .then((user) => res.json(user))
             .catch((err) => res.status(500).json(err));
     },
     createUser(req, res) {
-        User.findOne({})
+        User.create(req.body)
             .then((user) => res.json(user))
             .catch((err) => res.status(500).json(err));
     },
     updateUser(req, res) {
-        User.findOne({})
+        User.findByIdAndUpdate(req.params.userId, {$set:req.body}, {new: true})
             .then((user) => res.json(user))
             .catch((err) => res.status(500).json(err));
     },
     deleteUser(req, res) {
-        User.findOne({})
+        User.findByIdAndDelete(req.params.userId)
             .then((user) => res.json(user))
             .catch((err) => res.status(500).json(err));
     },
+    addFriend(req, res) {
+        User.findByIdAndUpdate(req.params.userId, {$addToSet:{friends:req.params.friendId}}, {new: true})
+            .then((user) => res.json(user))
+            .catch((err) => res.status(500).json(err));
+    },
+    deleteFriend(req, res) {
+        User.findByIdAndUpdate(req.params.userId, {$pull:{friends:req.params.friendId}}, {new: true})
+            .then((user) => res.json(user))
+            .catch((err) => res.status(500).json(err));
+    }
 
 }
